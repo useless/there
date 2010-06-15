420afc6f407974f56281bd468e427d4a3c28392f
Christoph Petrick <xlii@gmx.de>
2010-06-15 23:00:27 +0200

    Add basic functionality

---
+++ there.js
@@ -0,0 +1,69 @@
+function didLocate(position) {
+	setActivityStatus('done');
+	if(position.coords) {
+		var coo = position.coords;
+		setPosVal(coo.latitude, coo.longitude, '±'+coo.accuracy, true);
+	} else {
+		setPosVal('??','??','Invalid');
+	}
+}
+
+function locateDidFail(error) {
+	var reason='‽';
+	setActivityStatus('stop');
+
+	switch(error.code) {
+	case error.TIMEOUT:
+		reason = 'TIMEOUT';
+	break;
+	case error.PERMISSION_DENIED:
+		reason = 'DENIED';
+	break;
+	}
+	setPosVal('--','--', reason);
+}
+
+function setActivityStatus(state) {
+	document.getElementById('circles').setAttribute('class', state);
+}
+
+function setPosVal(latitude, longitude, accuracy, isValid) {
+	setTextForNodeId(latitude, 'lat');
+	setTextForNodeId(longitude, 'long');
+	setTextForNodeId(accuracy, 'acc');
+	if(isValid) {
+		document.getElementById('c0').setAttribute('href','http://maps.google.com/?q='+latitude+','+longitude+'+(There)');
+	} else {
+		document.getElementById('c0').setAttribute('href','');
+	}
+}
+
+function setTextForNodeId(text, nodeid) {
+	var node = document.getElementById(nodeid);
+	if(node) {
+		clearNode(node);
+		node.appendChild(document.createTextNode(text));
+	}
+}
+
+function clearNode(node) {
+	while(node.firstChild) {
+		node.removeChild(node.firstChild);
+	}
+}
+
+function startLocating() {
+	setActivityStatus('pre');
+	setPosVal('--','--','--');
+	if(navigator.geolocation) {
+		setActivityStatus('pulse');
+		setPosVal('…','…','…');
+		navigator.geolocation.getCurrentPosition(didLocate, locateDidFail,
+			{timeout:10000});
+	}
+}
+
+window.addEventListener('load', function() {
+	document.getElementById('acc').addEventListener('click', startLocating, false);
+	startLocating();
+}, false);
